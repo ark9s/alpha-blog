38ef47619cc6a4a72f394f40724972ea25dd7a79
Shangyun Wang <syw@Shangyuns-MacBook-Pro.local>
2021-03-30 16:14:55 -0700

    Finish function part

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,52 @@
 class ArticlesController < ApplicationController
+
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
   def show
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(get_valid_params)
+    if @article.save
+      flash.notice = "Article is successfully saved!"
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
+
+  def update
+    if @article.update(get_valid_params)
+      flash.notice = "Article is successfully updated!"
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def find_article
     @article = Article.find(params[:id])
+  end
+
+  def get_valid_params
+    params.require(:article).permit(:title, :description)
   end
 end
